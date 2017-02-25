5b0cb310615e05928c7e468939621f208a9d7d28
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-02-24 16:01:32 -0800

    fixing casing

--- OriginalSource/Sayedha.StarterWeb/Controllers/HomeController.cs
+++ OriginalSource/Sayedha.StarterWeb/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Sayedha.StarterWeb.Controllers
+namespace SayedHa.StarterWeb.Controllers
 {
     public class HomeController : Controller
     {
--- OriginalSource/Sayedha.StarterWeb/Startup.cs
+++ OriginalSource/Sayedha.StarterWeb/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Sayedha.StarterWeb
+namespace SayedHa.StarterWeb
 {
     public class Startup
     {
